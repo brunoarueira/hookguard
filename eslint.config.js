396b59dbee6466b3ba6353d2ea1bb13d9f25322d
Bruno Arueira <contato@brunoarueira.com>
2026-09-18 12:27:12 -0300

    Add TypeScript/Fastify project scaffolding
    npm project config (strict TS, ESLint flat config, Vitest), env var
    template, and gitignore for the hookguard service.

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+];
